ff983f3f8a2bba4dfa92bda9c4cbe359b32c66a1
Zezerino de Oliveira de Mattos <mattoszz@hotmail.com>
2022-10-16 15:15:16 -0300

    Criada a pagihome e sua rota

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,11 +4,13 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 // MEUS IMPORTS
 import Login from '../views/login';
 import NovoUsuario from '../views/novo-usuario';
+import Home from '../views/home';
 
 export default function Rotas(){
     return(
         <BrowserRouter>
             <Routes>
+                <Route path='/' element={<Home />} />
                 <Route path='/login' element={<Login />} />
                 <Route path='/novousuario' element={<NovoUsuario />} />
             </Routes>
