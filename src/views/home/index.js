b628b2a7cc0646cb8bc76c0ae79bea5b07c3495e
zezerinodev <mattoszz@hotmail.com>
2022-10-17 19:06:33 -0300

    Header finalizado

--- src/views/home/index.js
+++ src/views/home/index.js
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 // MEUS IMPORTS
 import './style.css';
+import imgCapaHeader from '../../assets/img-nanda-fashion003.jpg';
 import Navbar from '../../components/navbar';
 
 function Home(){
+    
+
     return(
         <>
-            <header className="">
+            <header >
                 <Navbar />
-                <div className='container-fluid banner-principal'></div>
-                <h1>teste</h1>
+                <div className='container-fluid p-0 banner-principal'>
+                    <img src={imgCapaHeader} class="img-fluid banner-principal-img " alt="Imagem Capa" />
+                    {
+                        window.innerWidth > 800 ? <div class="banner-principal-saiba-mais-grende">
+                                <a className="grande" href="">Saiba Mais</a>
+                            </div>
+                        :
+                        null
+                    }
+                </div>
             </header>
         </>
     );
