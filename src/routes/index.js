2d08aba5d0c5a4e06a441844c21cc7c8e1abd621
Zezerino de Oliveira de Mattos <mattoszz@hotmail.com>
2022-10-17 22:19:18 -0300

    Preparei a pagina de Rotas para armazenar o store.

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 // MEUS IMPORTS
+import store from '../store';
+
 import Login from '../views/login';
 import NovoUsuario from '../views/novo-usuario';
 import Home from '../views/home';
 
 export default function Rotas(){
     return(
-        <BrowserRouter>
-            <Routes>
-                <Route path='/' element={<Home />} />
-                <Route path='/login' element={<Login />} />
-                <Route path='/novousuario' element={<NovoUsuario />} />
-            </Routes>
-        </BrowserRouter>
-    )
+        <Provider store={store}>
+            <BrowserRouter>
+                <Routes>
+                    <Route path='/' element={<Home />} />
+                    <Route path='/login' element={<Login />} />
+                    <Route path='/novousuario' element={<NovoUsuario />} />
+                </Routes>
+            </BrowserRouter>
+        </Provider>
+    );
 }
