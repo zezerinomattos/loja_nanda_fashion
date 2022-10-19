a49ae3903f5c614ceb66c02bfcdae95170db2f5c
zezerinodev <mattoszz@hotmail.com>
2022-10-19 18:04:40 -0300

    Componente produto card finalizado

--- src/components/produtos-card/index.js
+++ src/components/produtos-card/index.js
@@ -5,10 +5,47 @@ import { Link, Navigate } from 'react-router-dom';
 
 // MEUS IMPORTS
 import './style.css';
+import firebase from '../../config/firebase';
 
-function ProdutoCard(){
+function ProdutoCard({key, id, titulo, descricao, promocao, valor, img, visualizacoes}){
+
+    const [urlImagem, setUrlImagem] = useState();
+    
+    useEffect(() => {
+        firebase.storage().ref(`imagens/${img}`).getDownloadURL()
+                .then(url => setUrlImagem(url));       
+    }, [urlImagem]);
+
     return(
-        <h1>componente</h1>
+
+        <div className="col-md-3 col-sm-12 ">
+            <div className="card-img-mestre">
+                <img src={urlImagem} className="card-img-top img-cartao" alt={img} />
+            </div>
+                                 
+            <div className="card-body">
+                <h5 className="card-title mt-2 mb-2 card-titulo" >{titulo}</h5>
+                <p className="card-text text-justify">{descricao}</p>
+                <div className="valores d-flex mb-3">
+                    <span className="valor-por me-2">R${promocao}</span>
+                    <div className="valor-mestre-de">
+                        <hr/>
+                        <span className="valor-de ms-2">R${valor}</span>
+                    </div>
+                </div>
+                <div className="row rodape-card dflex align-items-center">
+                    <div className="col-6 ">
+                        <Link to={'/produtosdetalhes/' + id} className="btn btn-sm btn-detalhes mb-2">+DETALHES</Link>
+                    </div>
+
+                    <div className='col-6 text-right rodape-visualizacoes'>
+                        <i class="fas fa-eye"></i> <span>{visualizacoes}</span>
+                    </div>
+                </div>
+                
+            </div>
+
+        </div>
     );
 }
 
--- src/views/home/index.js
+++ src/views/home/index.js
@@ -1,17 +1,38 @@
 import React, {useEffect, useState} from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 // MEUS IMPORTS
 import './style.css';
+import firebase from '../../config/firebase';
 import imgCapaHeader from '../../assets/img-nanda-fashion003.jpg';
 import Navbar from '../../components/navbar';
 import ProdutoCard from '../../components/produtos-card';
 
 function Home(){
     
+    const [produto, setProduto] = useState([]);
+    let listaProdutos = [];
+
+    useEffect(() => {
+        firebase.firestore().collection('nandaFashion').get()
+        .then(async (resultado) => {
+            await resultado.docs.forEach(doc => {
+                listaProdutos.push({
+                    id: doc.id,
+                    ...doc.data()
+                })
+            })
+            setProduto(listaProdutos);
+        })
+        .catch(erro => {
+            alert(erro);
+        });
+    });
 
     return(
-        <>
+        <div>
             <header >
                 <Navbar />
                 <div className='container-fluid p-0 banner-principal'>
@@ -19,11 +40,15 @@ function Home(){
                 </div>
             </header>
 
-            <ProdutoCard />
+            <section className='row produto-ofertas'>
+                <h2>OFERTAS</h2>
+                <div className='row produto-ofertas-mestre'>
+                    {produto.map(item => item.secaoDestacar === "Ofertas" ? <ProdutoCard id={item.id} titulo={item.nomeProduto} descricao={item.descricao} 
+                                promocao={item.promocao} valor={item.valor} img={item.imagem} visualizacoes={item.visualizacoes} /> : null )}
+                </div>
+            </section>
 
-            <h1>{useSelector(state => state.usuarioLogado)}</h1>
-            <h1>Logado: {useSelector(state => state.usuarioEmail)}</h1>
-        </>
+        </div>
     );
 }
 
