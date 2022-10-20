9f03904ab126f1a1a11df62696d2cf0ce81bdccb
Zezerino de Oliveira de Mattos <mattoszz@hotmail.com>
2022-10-19 22:10:45 -0300

    Colocando titulo no footer

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -17,27 +17,33 @@ function Footer(){
     return(
         <footer class="container-fluid">
             <div className='container my-3 footer-mestre'>
-                <ul class="nav flex-column">
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Produtos</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Contato</a>
-                    </li>
-                </ul>
+                <div className='flex-columm'>
+                    <h1 className='h5 mt-3'>NAVEGAÇÃO</h1>
+                    <ul class="nav flex-column">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Produtos</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Contato</a>
+                        </li>
+                    </ul>
+                </div>
 
-                <ul className='d-flex nav mt-2 cartoes-credito'>
-                    <li className='mx-2 my-2 '><img src={visa} alt="imagem de cartão de credito" /></li>
-                    <li><img src={master} alt="imagem de cartão de credito" /></li>
-                    <li><img src={amex} alt="imagem de cartão de credito" /></li>
-                    <li><img src={elo} alt="imagem de cartão de credito" /></li>
-                    <li><img src={hipper} alt="imagem de cartão de credito" /></li>
-                    <li><img src={pix} alt="imagem de cartão de credito" /></li>
-                    <li><img src={samsung} alt="imagem de cartão de credito" /></li>
-                </ul>
+                <div className='flex-columm'>
+                    <h1 className='h5 mt-3'>NAVEGAÇÃO</h1>
+                    <ul className='d-flex nav mt-2 cartoes-credito'>
+                        <li className='mx-2 my-2 '><img src={visa} alt="imagem de cartão de credito" /></li>
+                        <li><img src={master} alt="imagem de cartão de credito" /></li>
+                        <li><img src={amex} alt="imagem de cartão de credito" /></li>
+                        <li><img src={elo} alt="imagem de cartão de credito" /></li>
+                        <li><img src={hipper} alt="imagem de cartão de credito" /></li>
+                        <li><img src={pix} alt="imagem de cartão de credito" /></li>
+                        <li><img src={samsung} alt="imagem de cartão de credito" /></li>
+                    </ul>
+                </div>
             </div>
             <p className='mt-5'><a class="nav-link" href="https://zezerino.com.br/">Copyright ©2022 Zezerino Dev</a></p>
         </footer>
