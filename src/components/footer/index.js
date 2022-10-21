15a4efb86acce03905c5e3bb3f583ae0b72896f4
Zezerino de Oliveira de Mattos <mattoszz@hotmail.com>
2022-10-20 21:45:49 -0300

    footer tela grande finalizada

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -17,7 +17,7 @@ function Footer(){
     return(
         <footer class="container-fluid">
             <div className='container my-3 footer-mestre'>
-                <div className='flex-columm'>
+                <div className='flex-columm footer-mestre-nav'>
                     <h1 className='h5 mt-3'>NAVEGAÇÃO</h1>
                     <ul class="nav flex-column">
                         <li class="nav-item">
@@ -32,7 +32,7 @@ function Footer(){
                     </ul>
                 </div>
 
-                <div className='flex-columm'>
+                <div className='footer-mestre-cartoes'>
                     <h1 className='h5 mt-3'>NAVEGAÇÃO</h1>
                     <ul className='d-flex nav mt-2 cartoes-credito'>
                         <li className='mx-2 my-2 '><img src={visa} alt="imagem de cartão de credito" /></li>
@@ -44,6 +44,24 @@ function Footer(){
                         <li><img src={samsung} alt="imagem de cartão de credito" /></li>
                     </ul>
                 </div>
+
+                <div className='flex-columm footer-mestre-nav'>
+                    <h1 className='h5 mt-3'>CONTATO</h1>
+                    <ul class="nav flex-column">
+                        <li class="nav-item">
+                            <i class="fa-brands fa-whatsapp"></i> 48991589625
+                        </li>
+                        <br/>
+                        <li class="nav-item">
+                            <i class="fa-solid fa-envelope"></i> nanda.buenop@homail.com
+                        </li>
+                        <br/>
+                        <li class="nav-item">
+                            <i class="fa-solid fa-location-dot"></i> Rua 4 quadra E Lote 5 Araranguá-SC Brasil
+                        </li>
+                    </ul>
+                </div>
+
             </div>
             <p className='mt-5'><a class="nav-link" href="https://zezerino.com.br/">Copyright ©2022 Zezerino Dev</a></p>
         </footer>
