af1bd433ed5ef24c86ae9aa5b574c24afe7e5970
Zezerino de Oliveira de Mattos <mattoszz@hotmail.com>
2022-10-22 18:52:40 -0300

    Tela de contato criada e inicializada

--- src/views/contato/index.js
+++ src/views/contato/index.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 //MY IMPORTS
@@ -6,10 +7,21 @@ import './style.css';
 import Navbar from '../../components/navbar';
 
 function Contato(){
+
     return(
         <>
             <Navbar />
-            <h1>contato</h1>
+            <main className='contato-mestre'>
+                <h1 className='h1 text-center my-5'>CONTATO</h1>
+                <div className='contato-mestre'>
+                    <div className='mapa-mestre'>
+                        <div className='mapa'>
+                                                    
+                        </div>
+                    </div>
+                    <div className='form-mestre'></div>
+                </div>
+            </main>
         </>
     );
 }
--- src/views/produtos/index.js
+++ src/views/produtos/index.js
@@ -14,6 +14,8 @@ function Produtos(){
     const [produtos, setProduto] = useState([]);
     let listaProdutos = [];
 
+    const [pesquisa, setPesquisa] = useState('');
+
     useEffect(() => {
         firebase.firestore().collection('nandaFashion').get()
         .then(async (resultado) => {
@@ -31,6 +33,12 @@ function Produtos(){
         <>
             <Navbar />
             <main className='my-5'>
+
+                <form class="d-flex my-5 mx-auto col-11">
+                    <input onChange={(e) => setPesquisa(e.target.value.toUpperCase())} class="form-control me-2 pesquisar" type="search" placeholder="Pesquisar" />
+                </form>
+
+
                 <section className='produtos-mestre row'>
                     {produtos.map(item => <ProdutoCard id={item.id} titulo={item.nomeProduto} descricao={item.descricao} 
                             promocao={item.promocao} valor={item.valor} img={item.imagem} />)}
