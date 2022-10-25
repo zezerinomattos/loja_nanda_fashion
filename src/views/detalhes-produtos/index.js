52c69007191b2274096a1b32521bd1306505a9b5
Zezerino de Oliveira de Mattos <mattoszz@hotmail.com>
2022-10-24 22:21:21 -0300

    lyout detalhes de produtos finalizado.

--- src/views/detalhes-produtos/index.js
+++ src/views/detalhes-produtos/index.js
@@ -14,9 +14,9 @@ function DetalhesProdutos(){
     return(
         <>
             <Navbar />
-            <div className='container-fluid my-4'>
+            <div className='container-fluid secao-detalhes-produtos my-4'>
                 <div className='row detalhes-mestre col-12'>
-                    <h1 className='h2 my-3 text-center titulo'>DETALHES DO PRODUTO</h1>
+                    <h1 className='h2 my-4 text-center titulo'>DETALHES DO PRODUTO</h1>
                     <div className='detalhes-mestre-img col-md-6 col-sm-12'>
                         <div className='container'>
                             <img src="https://via.placeholder.com/150x100" className='img-fluid my-2' alt="" />
@@ -30,8 +30,28 @@ function DetalhesProdutos(){
                             com isso evitamos um atendimento frio e sem emoção, com nosso atendimento personalizado nossos clientes podem tirar suas
                             duvidas, como por exemplo, o tamanho de confecção, cores mais realistas e muitas outras informações. 
                         </p>
+
+                        <div className='my-5 botao-comprar'>
+                            <a href="#">COMPRAR</a>
+                        </div>
+                        
+                        <h3 className='h5'>COMPARTILHAR:</h3>
+                        <div className='redes-sociais'>
+                            <a className='mx-1' href=""><i class="fa-brands fa-instagram"></i></a>
+                            <a className='mx-1' href=""><i class="fa-brands fa-facebook"></i></a>
+                            <a className='mx-1' href=""><i class="fa-brands fa-whatsapp"></i></a>
+                        </div>
+
                     </div>
                 </div>
+
+                <div className='mx-auto descricao-mestre'>
+                    <h3 className='my-3'>DESCRIÇÃO</h3>
+                    <p>
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                    </p>
+                </div>
+
             </div>
             <Footer />
         </>
