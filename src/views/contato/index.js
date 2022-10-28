8c560ef048243cc6586d53ee95b76f1769f8e83f
Zezerino de Oliveira de Mattos <mattoszz@hotmail.com>
2022-10-28 12:38:29 -0300

    Criando Mapa da pagina de Contato

--- src/views/contato/index.js
+++ src/views/contato/index.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { GoogleMap, useJsApiLoader, Marker } from '@react-google-maps/api';
 
 
 //MY IMPORTS
@@ -8,6 +9,16 @@ import Navbar from '../../components/navbar';
 
 function Contato(){
 
+    const { isLoaded } = useJsApiLoader({
+        id: 'google-map-script',
+        googleMapsApiKey: "AIzaSyDHwHEWkp6vfhPRshAd_w77YO6nj1GJor4"
+    })
+
+    const position = {
+        lat: -28.948874, 
+        lng: -49.534056
+    }
+
     return(
         <>
             <Navbar />
@@ -15,8 +26,26 @@ function Contato(){
                 <h1 className='h1 text-center my-5'>CONTATO</h1>
                 <div className='contato-mestre'>
                     <div className='mapa-mestre'>
-                        <div className='mapa'>
-                                                    
+                        <div className='map'>
+                            {
+                                isLoaded ? (
+                                    <GoogleMap
+                                      mapContainerStyle={{width: '100%', height: '100%'}}
+                                      center={position}
+                                      zoom={16}
+                                    >
+                                    <Marker 
+                                        position={position} 
+                                        options={{
+                                            label: {
+                                                text: "Nanda Fashion",
+                                                className: "map-marker"
+                                            }
+                                        }}
+                                    />
+                                    </GoogleMap>
+                                ) : <></>
+                            }               
                         </div>
                     </div>
                     <div className='form-mestre'></div>
